3c5871d5d2a400fb5cb308106dc5d7e2c72dd61c
José Miguel <a19esrujo@iesgrancapitan.org>
2023-09-18 17:32:25 +0200

    add: variables y hola mundo

---
+++ holaMundo.py
@@ -0,0 +1,13 @@
+"""
+Primer programa en Python
+Autor: José Miguel Escribano Ruiz
+Fecha: 18/09/2023
+Descripción: Programa que imprime un mensaje en pantalla
+"""
+
+print("Hola Mundo")
+
+# consultar el tipo de dato de un dato
+print(type(5)) # int
+print(type(5.5)) # float
+
---
+++ variables.py
@@ -0,0 +1,23 @@
+"""
+Variables
+Autor: José Miguel Escribano Ruiz
+Fecha: 18/09/2023
+Descripción: Programa que muestra el uso de variables
+
+"""
+
+# Declaración de variables
+x = 5
+y = 3.1415
+print(x)
+print(y)
+
+nombre = "José Miguel"
+apellido = "Escribano Ruiz"
+print(nombre)
+print(apellido)
+
+nombre = input("Introduce tu nombre: ")
+print("Hola " + nombre)
+
+print(sum([1, 2, 3, 4, 5]))
