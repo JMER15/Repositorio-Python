9a42b9d81e7c7949c8f19bc4b28286c61c76e6b8
José Miguel <a19esrujo@iesgrancapitan.org>
2023-10-03 12:30:38 +0200

    add: challengue 39 mouredev

--- python_medio/03_challenges.py
+++ python_medio/03_challenges.py
@@ -49,3 +49,38 @@ def anagrama(string1, string2):
     
 print(anagrama("roma", "amor"))
 print(anagrama("roma", "amorr"))
+
+"""
+Crea una función que encuentre todos los triples pitagóricos
+(ternas) menores o iguales a un número dado.
+ - Debes buscar información sobre qué es un triple pitagórico.
+ - La función únicamente recibe el número máximo que puede
+   aparecer en el triple.
+ - Ejemplo: Los triples menores o iguales a 10 están
+   formados por (3, 4, 5) y (6, 8, 10).
+"""
+
+def triples_pitagoricos(number):
+    for a in range(1,number+1): # el +1 es para que incluya el número que le pasamos
+        for b in range(a,number+1):
+            for c in range(b,number+1):
+                if a**2 + b**2 == c**2:
+                    print(a,b,c)
+
+### otra solucion quitando el bucle de c
+def triples_pitagoricos1(number):
+    triples = []
+    for a in range(1, number+1):
+        for b in range(a,number+1):
+            c_cuadrado = a**2+b**2
+            c = int(c_cuadrado**0.5)
+            if c <= number and a**2 + b**2 == c**2: # c tiene que ser menor e igual a number 
+                triples.append((a,b,c))
+    return triples
+
+triples_pitagoricos(20)
+result = triples_pitagoricos1(20)
+
+for triple in result:
+    print(triple)
+
